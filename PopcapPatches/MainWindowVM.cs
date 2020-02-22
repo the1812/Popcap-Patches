e51aabf218b1deb37cc789d24c96bd6266a60a5b
the1812 <the1812@outlook.com>
2020-02-22 22:41:47 +0800

    添加项目文件。

---
+++ PopcapPatches/MainWindowVM.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopcapPatches
+{
+  sealed class MainWindowVM : INotifyPropertyChanged
+  {
+    private readonly MainWindow window;
+    public MainWindowVM(MainWindow window)
+    {
+      this.window = window;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    private void OnPropertyChanged(string name)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+
+    private string log = "Ready";
+    public string Log
+    {
+      get => log;
+      set
+      {
+        log = value;
+        OnPropertyChanged(nameof(Log));
+      }
+    }
+    public void AddLog(string log)
+    {
+      Log += $"\n{log}";
+    }
+
+
+    private string filename = "<No file selected>";
+    public string Filename
+    {
+      get => filename;
+      set
+      {
+        filename = value;
+        OnPropertyChanged(nameof(Filename));
+        OnPropertyChanged(nameof(CanPatch));
+      }
+    }
+    public bool CanPatch => !Filename.StartsWith("<") && NotBusy;
+    public bool RemoveVideoMemoryCheck { get; set; } = true;
+    public bool RemoveVideoCardCheck { get; set; } = true;
+
+
+    private string fileFullPath = "";
+    public void SelectFile()
+    {
+      var dialog = new OpenFileDialog
+      {
+        Title = "Select file to patch",
+        Filter = "Executable files (*.exe)|*.exe|All files (*.*)|*.*",
+        DefaultExt = ".exe",
+      };
+      if (dialog.ShowDialog(window) ?? false)
+      {
+        fileFullPath = dialog.FileName;
+        Filename = Path.GetFileName(fileFullPath);
+      }
+    }
+
+
+    private bool notBusy = true;
+    public bool NotBusy
+    {
+      get => notBusy;
+      set
+      {
+        notBusy = value;
+        OnPropertyChanged(nameof(NotBusy));
+        OnPropertyChanged(nameof(CanPatch));
+      }
+    }
+
+    public async void RunPatch()
+    {
+      await Task.Run(() =>
+      {
+        try
+        {
+          NotBusy = false;
+          Log = "";
+          AddLog("Start patching...");
+          AddLog($"File: {Filename}");
+          var suffix = "-nosig";
+          var patch = new Patch(File.ReadAllBytes(fileFullPath));
+          AddLog("Remove signature check");
+          patch.RemoveSignatureCheck();
+          if (RemoveVideoMemoryCheck)
+          {
+            suffix += "+vmem";
+            AddLog("Remove video memory check");
+            patch.RemoveVideoMemoryCheck();
+          }
+          if (RemoveVideoCardCheck)
+          {
+            suffix += "+vard";
+            AddLog("Remove video card check");
+            patch.RemoveVideoCardCheck();
+          }
+          var outputFilename = $"{Path.GetFileNameWithoutExtension(Filename)}{suffix}{Path.GetExtension(Filename)}";
+          var outputPath = Path.Combine(Path.GetDirectoryName(fileFullPath), outputFilename);
+          AddLog($"Output: {outputFilename}");
+          File.WriteAllBytes(outputPath, patch.Output);
+          AddLog($"Patch completed");
+        }
+        catch (PatchException ex)
+        {
+          AddLog($"Error: {ex.Message}");
+        }
+        finally
+        {
+          NotBusy = true;
+        }
+      });
+    }
+
+  }
+}
---
+++ PopcapPatches/Patch.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace PopcapPatches
+{
+  sealed class PatchException: Exception
+  {
+    public PatchException()
+    {
+    }
+    public PatchException(string message): base(message) 
+    {
+    }
+    public PatchException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+  }
+  sealed class Patch
+  {
+    private string inputString;
+    public byte[] Output { get; private set; }
+    public Patch(byte[] input)
+    {
+      var hex = new StringBuilder(input.Length * 2);
+      foreach (var b in input)
+      {
+        hex.AppendFormat("{0:X2}", b);
+      }
+      inputString = hex.ToString();
+      Debug.WriteLine("inputString: " + inputString.Substring(0, 36));
+      Output = new byte[input.Length];
+      input.CopyTo(Output, 0);
+    }
+    sealed class BytePattern
+    {
+      public string[] Pattern { get; set; }
+      public int Match(string data)
+      {
+        var regexText = Pattern.Select(p =>
+        {
+          if (p == "*")
+          {
+            return ".{2}";
+          }
+          return p.ToUpperInvariant();
+        }).Aggregate((acc, it) => acc + it);
+        var regex = new Regex(regexText);
+        var matches = regex.Matches(data);
+        if (matches.Count == 0)
+        {
+          throw new PatchException("No match in data");
+        }
+        if (matches.Count > 1)
+        {
+          throw new PatchException("Too many matches in data");
+        }
+        return matches[0].Index / 2;
+      }
+    }
+    public void RemoveSignatureCheck()
+    {
+      var pattern = new BytePattern
+      {
+        Pattern = new string[]
+        {
+          "C.{1}", "FF", "D2", "84", "C0",
+          "0F", "*", "*", "*", "00", "00", "68",
+        }
+      };
+      var index = pattern.Match(inputString);
+      Output[index + 5] = Convert.ToByte("E9", 16);
+      Output[index + 6] = (byte)(Output[index + 7] + 1);
+      Output[index + 7] = Output[index + 8];
+      Output[index + 8] = 0;
+      Output[index + 10] = Convert.ToByte("90", 16); ;
+    }
+    public void RemoveVideoMemoryCheck()
+    {
+      var pattern = new BytePattern
+      {
+        Pattern = new string[]
+        {
+          "FF", "C1", "E8", "14", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*",
+          "73",
+        }
+      };
+      var index = pattern.Match(inputString) + 16;
+      Output[index] = Convert.ToByte("EB", 16);
+    }
+    public void RemoveVideoCardCheck()
+    {
+      var pattern = new BytePattern
+      {
+        Pattern = new string[]
+        {
+          "8B", "*", "50", "EB", "03", "8D", "*", "*", "*", "*", "*", "*", "FF", "FF", "84", "C0",
+          "75",
+        }
+      };
+      var index = pattern.Match(inputString) + 16;
+      Output[index] = Convert.ToByte("EB", 16);
+    }
+  }
+}
